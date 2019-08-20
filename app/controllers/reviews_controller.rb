512cace993af286d6b2dc8e95c4a9b1bfca5c3d6
Student <example@example.com>
2019-08-20 10:03:54 -0700

    This is for to GET and POST reviews about travel destinations and  to GET reviews by country or city.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -31,6 +31,7 @@ class ReviewsController < ApplicationController
   def update
     @review = Review.find(params[:id])
     @review.update(review_params)
+    json_response(@review)
   end
 
   def destroy
